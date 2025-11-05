e5e22181eade8575a81f48400cddb95d01d793c6
Dev Dobariya <devdobariya75@gmail.com>
2025-11-05 05:47:12 +0000

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -16,7 +16,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
 // "" => false
+// "Dev" => true 
 
 let someNumber = 33
 let stringsomeNumber = String(someNumber)
 console.log(typeof someNumber)
+
+
+//************** Opeerations ***************** 
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+
+// --> can perform all arithmetic opeartions
+
+
+// let str1 = "Dev"
+// let str2 = "Dev Dobariya"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32
+
+let num1 , num2 , num3
+
+// readbility is must 
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+
+
+// preIncreament and PostIncrement
+
