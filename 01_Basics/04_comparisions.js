cb2fe868a999672c6d0062b3ff3316256aa682e4
Dev Dobariya <devdobariya75@gmail.com>
2025-11-05 05:54:45 +0000

    comparisions

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("2" < 1)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+ 
+//same can check for undefined 
+
+// == and ===
+console.log("2" === 2);
+
+
