464557eea9819c8aedeaa79f118d8e83f4c8f271
Dev Dobariya <devdobariya75@gmail.com>
2025-11-05 05:15:23 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 14453;
+let accountEmail = "dev123@gmail.cpm"
+var accountpassword = "12345"
+accountCity = "surat"
+let accountState
+
+// accountId = 2 -> not allowed 
+
+
+/*
+prefer not to use var
+
+*/
+
+accountEmail = "dobariya@gmail.com"
+accountpassword = "1111"
+accountCity = "gandhinagar"
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountpassword , accountCity])
