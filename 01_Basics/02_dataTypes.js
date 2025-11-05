22b7bd333115893349387f063438c4496dd0ff5e
Dev Dobariya <devdobariya75@gmail.com>
2025-11-05 05:27:19 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello"); --> we are using node js
+
+console.log(3 + 3)
+
+let name = "hitesh"
+let age = 18
+let isLoffedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/fasle
+// null => standalone value
+// undefined => not assigned anything
+// symbol => unique 
+
+
+//object 
+
+console.log(typeof "hitesh")
+// typeof --> used to know datatype 
+
+console.log(typeof null) // object
