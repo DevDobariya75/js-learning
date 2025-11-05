a95fcd11843d2b473b3d0faac6315fd024fb141f
Dev Dobariya <devdobariya75@gmail.com>
2025-11-05 06:24:53 +0000

    datatype summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// primitive datatypes
+// 7 types : String  , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 78945612365454n
+
+// Dynamically typed language
+
+// Reference (Non primitive)
+// Array , Objects , Functinos
+
+const heros = ["shktiman" , "doga" , "iornman"]
+
+let muOgj = {
+    name: "dev",
+    age: 20
+}
+
+const myFunction = function(){
+   console.log("hello word");    
+}
+
+// check data type of all using typeof
