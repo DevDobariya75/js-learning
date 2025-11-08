ebeadb518d80a9a4cd3aa156fe9954b8783aa4a2
DevDobariya75 <devdobariya75@gmail.com.com>
2025-11-08 13:12:55 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+// let a = 10
+// const b = 20
+var c = 300
+
+if(true){
+    let a = 10 // blocked scope
+    const b = 20 // blocked scope
+    var c = 30
+}
+
+// global scope is different in console and vs code
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
+
