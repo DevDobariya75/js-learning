76c3e3ff6eb5dbb0510e10631cd2a5ea72cdc922
DevDobariya75 <devdobariya75@gmail.com.com>
2025-11-08 13:43:00 +0530

    scope in functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,8 +12,52 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+let d = 3;
+
+if(true){
+    //console.log("hiii" , d);
+}
 
 
+// nested functions
+function one(){
+    const username = "dev"
 
+    function two(){
+        const website = "youtueb"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
+//check scope -- understood
+if(true){
+    const username = "dev"
+    if(username == "dev"){
+        const website = "youtube"
+        console.log(username + website); 
+    }
+}
+
+// ++++++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+// console.log(addone(5));
+
+
+//holding function in variable
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(4));
 
