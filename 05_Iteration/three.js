5ec1c585c25eb3dc4730b6ce560647e87b8fc2c2
DevDobariya75 <devdobariya75@gmail.com.com>
2025-11-08 19:34:19 +0530

    diiferent type of array

---
+++ 05_Iteration/three.js
@@ -0,0 +1,38 @@
+// thode array specififc loop
+
+// for of------------------------
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "hello world"
+// for(const greet of greetings){
+//     console.log(greet); 
+// }
+
+
+// Maps-------------------------
+
+const map = new Map() // unique values and ordered
+map.set('IN' , "India")
+map.set('USA' , "United states of America")
+map.set('Fr' , "France")
+
+// console.log(map);
+
+// for(const [key , value] of map){
+//     console.log(key , ':-' , value);
+// }
+
+const myObj = {
+    'game1': 'NFS',
+    'gaem2': 'Spideman'
+}
+// not iteratabel using above method of for of
+
+6
+
+
+
